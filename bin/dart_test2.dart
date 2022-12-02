f24eb2516870a0b3c717e47a86f01939686ac411
williammunnich <williammunnich@pm.me>
2022-12-02 17:17:30 -0600

    list of strings and addition

--- bin/dart_test2.dart
+++ bin/dart_test2.dart
@@ -1,6 +1,21 @@
 import 'package:dart_test2/dart_test2.dart' as dart_test2;
 
+typedef StringList = List<String>;
+
+typedef Calculator(int first, int second);
+
+sum(int m, int n) {
+  print("Result of Sum: ${m + n}");
+}
+
 void main(List<String> arguments) {
+  StringList test1 = ["one", "two", "three"];
+  Calculator add = sum;
+
   print(
       'Hello world this is a test! Here is a calculation: ${dart_test2.calculate()}!');
+
+  print(test1);
+
+  add(10, 20);
 }
